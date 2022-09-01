4bc52919832947ea35cb9bdbb6b83588d26557cc
duqingquan <duqingquan@dcloud.io>
2022-09-01 19:27:09 +0800

    remove unuse module

--- uni_modules/DCloud-uts-batteryinfo/utssdk/mp-weixin/index.js
+++
@@ -1,9 +0,0 @@
-export default async function getBatteryInfo() {
-    return new Promise((resolve) => {
-        wx.getBatteryInfo({
-            success(res) {
-                resolve(res)
-            }
-        })
-    })
-}
