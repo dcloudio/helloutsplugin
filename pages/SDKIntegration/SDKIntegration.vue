b3beda8063ae3ef183183d2c26063a31ee35af52
duqingquan <duqingquan@dcloud.io>
2022-09-06 18:36:35 +0800

    uts location add

--- pages/SDKIntegration/SDKIntegration.vue
+++ pages/SDKIntegration/SDKIntegration.vue
@@ -1,11 +1,15 @@
 <template>
 	<view>
 		<page-head :title="title"></page-head>
-			
+		<view class="uni-btn-v uni-common-mt">
+			<button type="primary"  @tap="testGetlocation">获取定位信息</button>
+		</view>
 	</view>
 </template>
 
 <script>
+	 import {requestPremission,getLocation} from "@/uni_modules/uts-tencentgeolocation";
+	 
 	export default {
 		data() {
 			return {
@@ -13,7 +17,12 @@
 			}
 		},
 		methods: {
-			
+			testGetlocation:function(e){
+				
+				requestPremission();
+				var locationRet = getLocation();
+				console.log(locationRet);
+			},
 		}
 	}
 </script>
