56128bf5890fd80f0a733455cdc7ef28d5f73ac5
DCloud_Heavensoft <wanganxp@163.com>
2022-09-09 03:17:39 +0800

    Update SDKIntegration.vue

--- pages/SDKIntegration/SDKIntegration.vue
+++ pages/SDKIntegration/SDKIntegration.vue
@@ -1,6 +1,21 @@
 <template>
 	<view>
 		<page-head :title="title"></page-head>
+		<view class="uni-padding-wrap uni-common-mt">
+			<view class="uni-hello-text">
+				1. 腾讯定位sdk需在腾讯地图官网申请key。https://lbs.qq.com/mobile/androidMapSDK/
+				developerGuide/getKey。
+			</view>
+			<view class="uni-hello-text">
+				2. 将key配置在uts插件uts-tencentgeolocation/utssdk/app-android/AndroidManifest.xml中。
+			</view>
+			<view class="uni-hello-text">	
+				3. 打包自定义基座，注意将uts-tencentgeolocation/utssdk/app-android/libs/androix-core-1.0.0.jar先移到其他目录。
+			</view>
+			<view class="uni-hello-text">	
+				4. 打包自定义基座后将androix-core-1.0.0.jar移回原位，然后真机运行自定义基座。这个临时的移出移入问题后续会升级解决，详见uts插件开发文档的临时注意。
+			</view>
+		</view>
 		<view class="uni-btn-v uni-common-mt">
 			<button type="primary"  @tap="checkLocationPermission">请求定位权限</button>
 			<button type="primary"  @tap="testGetlocation">腾讯地图获取定位（需自定义基座）</button>
@@ -14,7 +29,7 @@
 	export default {
 		data() {
 			return {
-				title: 'SDK集成示例',
+				title: '腾讯定位SDK集成示例',
 			}
 		},
 		methods: {
