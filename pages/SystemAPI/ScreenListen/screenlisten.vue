3ed223b2549c147536f3c6eaeab7beccb51e8ef9
duqingquan <duqingquan@dcloud.io>
2022-10-20 18:27:33 +0800

    截屏监听插件 增加状态码，减少权限请求接口。与微信保持一致

--- pages/SystemAPI/ScreenListen/screenlisten.vue
+++ pages/SystemAPI/ScreenListen/screenlisten.vue
@@ -3,7 +3,6 @@
 		<page-head title="监听系统截屏" accordion></page-head>
 		
 		<uni-list>
-			<uni-list-item @tap="testScreenShotPremission" title="准备截屏监听权限(仅Android)" class="itemButton" :clickable="true"/>
 			<uni-list-item @tap="testScreenShotListen" title="监听截屏事件" class="itemButton" :clickable="true"/>
 			<uni-list-item @tap="testScreenShotOff" title="关闭截屏监听" class="itemButton" :clickable="true"/>
 		</uni-list>
@@ -15,7 +14,6 @@
 
 <script>
 	import {
-		requestPremission,
 		onUserCaptureScreen,
 		offUserCaptureScreen
 	} from "@/uni_modules/uts-screenshot-listener";
@@ -29,34 +27,29 @@
 	
 		methods: {
 
-			testScreenShotPremission() {
-				if (uni.getSystemInfoSync().platform == "ios") {
-					uni.showToast({
-						icon:"none",
-						title:'iOS 平台无需请求权限'
-					})
-					return;
-				}
-				// 请求写入储存的权限
-				requestPremission();
-			},
 			testScreenShotListen() {
 				var that = this;
 				onUserCaptureScreen(function(res) {
 						console.log(res);
-						uni.showToast({
-							icon:"none",
-							title:'捕获截屏事件'
-						})
-						if (uni.getSystemInfoSync().platform == "android") {
-							that.screenImage = res.image
+						if(res.code == -1){
+							// 启动失败
+							return ;
+						}else if(res.code == 0){
+							uni.showToast({
+								icon:"none",
+								title:'截屏监听已开启'
+							})
+						}else {
+							uni.showToast({
+								icon:"none",
+								title:'捕获截屏事件'
+							})
+							if (uni.getSystemInfoSync().platform == "android") {
+								that.screenImage = res.image
+							}
 						}
 					});
-				// 提示已经开始监听,注意观察
-				uni.showToast({
-					icon:"none",
-					title:'截屏监听已开启'
-				})
+				
 			},
 			testScreenShotOff() {
 				var that = this;
