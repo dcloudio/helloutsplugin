0805d1c07cb93aa3ae56421a9ddd70946cb6e841
XHY <xionghuayu@dcloud.io>
2022-11-22 13:02:48 +0800

    腾讯定位模块增加 监听位置变化，清除监听 方法

--- pages/SDKIntegration/TencentLocation/TencentLocation.vue
+++ pages/SDKIntegration/TencentLocation/TencentLocation.vue
@@ -3,24 +3,32 @@
 		<page-head :title="title"></page-head>
 		<view class="uni-padding-wrap uni-common-mt">
 			<view class="uni-hello-text">
-				1. 腾讯定位sdk需在腾讯地图官网申请key。https://lbs.qq.com/mobile/androidMapSDK/
-				developerGuide/getKey。
+				1. 腾讯定位sdk需在腾讯地图官网申请key。 https://lbs.qq.com/
 			</view>
 			<view class="uni-hello-text">
-				2. 将key配置在uts插件uts-tencentgeolocation/utssdk/app-android/AndroidManifest.xml中。
+				2. 按照readme文档配置apikey
 			</view>
-			
+			<view class="uni-hello-text">
+				3. 需要制作自定义基座运行
+			</view>
 		</view>
 		<view class="uni-btn-v uni-common-mt">
-			<button type="primary"  @tap="checkLocationPermission">请求定位权限</button>
-			<button type="primary"  @tap="testGetlocation">腾讯地图获取定位（需自定义基座）</button>
+			<button type="primary" @tap="checkLocationPermission">请求定位权限</button>
+			<button type="primary" @tap="testGetlocation">获取设备位置信息</button>
+			<button type="primary" @tap="testWatchPosition">监听设备位置信息</button>
+			<button type="primary" @tap="testClearWatch">停止监听</button>
 		</view>
 	</view>
 </template>
 
 <script>
-	 import {requestPremission,getLocation} from "@/uni_modules/uts-tencentgeolocation";
-	 
+	import {
+		requestPremission,
+		getLocation,
+		watchPosition,
+		clearWatch
+	} from "@/uni_modules/uts-tencentgeolocation";
+
 	export default {
 		data() {
 			return {
@@ -28,38 +36,60 @@
 			}
 		},
 		methods: {
-			
-			checkLocationPermission:function(e){
+
+			checkLocationPermission: function(e) {
 				requestPremission();
 			},
-			testGetlocation:function(e){
-				
+			testGetlocation: function(e) {
+
 				let startRet = getLocation({
-					geocode:true,
-					success:function(response){
+					geocode: true,
+					success: function(response) {
 						console.log(response);
 						var addressDesc = response.name + '-' + response.address
 						uni.showToast({
-							title:'执行结果:' + addressDesc,
-							icon:'none'
+							title: '执行结果:' + addressDesc,
+							icon: 'none'
 						});
 					},
-					fail:function(msg) {
+					fail: function(msg) {
 						uni.showToast({
 							title: msg,
-							icon:"none"
+							icon: "none"
 						})
 					}
 				})
-				
-				if(!startRet){
+
+				if (!startRet) {
 					uni.showToast({
-						title:'定位启动失败，请检查配置',
-						icon:'none'
+						title: '定位启动失败，请检查配置',
+						icon: 'none'
 					});
 				}
-				
+
 			},
+			testWatchPosition() {
+				watchPosition({
+					geocode: true,
+					success: function(response) {
+						console.log(response);
+						var addressDesc = response.name + '-' + response.address
+						uni.showToast({
+							title: '执行结果:' + addressDesc,
+							icon: 'none'
+						});
+					},
+					fail: function(msg) {
+						uni.showToast({
+							title: msg,
+							icon: "none"
+						})
+					}
+				})
+			},
+			testClearWatch() {
+				clearWatch()
+			}
 		}
 	}
 </script>
