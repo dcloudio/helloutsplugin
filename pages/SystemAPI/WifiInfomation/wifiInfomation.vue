b7cdd6c763ad18b0bb78e8ec68c0aacc3241988a
lizhongyi <lizhongyi@dcloud.io>
2022-11-25 11:42:10 +0800

    feat: add wifiInfomation 插件

---
+++ pages/SystemAPI/WifiInfomation/wifiInfomation.vue
@@ -0,0 +1,53 @@
+<template>
+	<page-head :title="title"></page-head>
+	<view class="uni-padding-wrap uni-common-mt">
+		<button @tap="getLocationPromise">获取定位权限</button>
+		<button @tap="getWifiInfo">获取WiFi信息</button>
+	</view>
+</template>
+
+<script>
+	
+import { getCurrentWifiInfo, requestLocationPromise } from '../../../uni_modules/uni-wifiinformation/utssdk/app-ios/index.uts'
+
+	export default {
+		
+		data() {
+			return {
+				title:"获取当前连接wifi信息"
+			}
+		},
+		methods:{
+			
+			getLocationPromise() {
+				requestLocationPromise((res):void => {
+					console.log(res)
+					uni.showToast({
+						title: res["message"],
+						icon: 'none'
+					})
+				})
+			},
+			
+			getWifiInfo() {
+				/* iOS12之后获取wifi信息需要获取系统定位权限，否则获取到的wifi信息为空 */
+				requestLocationPromise((res):void => {
+					if (res["success"] == true) {
+						let wifiInfo = getCurrentWifiInfo()
+						if (wifiInfo != null) {
+							console.log(wifiInfo)
+							uni.showToast({
+								title: "当前wifi信息获取成功",
+								icon: 'none'
+							})
+						}
+					}
+				})
+			}
+			
+		}
+	}
+</script>
+
+<style>
+</style>
