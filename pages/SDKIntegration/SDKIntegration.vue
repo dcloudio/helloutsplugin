f231a8304cf56754c085fa1d0dd231095b50e2ee
duqingquan-jpc <zmobs0@163.com>
2022-12-20 18:54:46 +0800

    提交 uts-lottie示例

--- pages/SDKIntegration/SDKIntegration.vue
+++ pages/SDKIntegration/SDKIntegration.vue
@@ -4,6 +4,7 @@
 		<uni-list>
 			<uni-list-item @tap="gotoTencentLocation" title="腾讯定位sdk集成示例" class="itemButton" :clickable="true" link/>
 			<uni-list-item @tap="gotoToast" title="Toast示例" class="itemButton" :clickable="true" link/>
+			<uni-list-item @tap="gotoLottie" title="Lottie动画示例" class="itemButton" :clickable="true" link/>
 		</uni-list>
 	</view>
 </template>
@@ -13,6 +14,10 @@
 	 import {
 	 	checkHasIntegration
 	 } from "@/uni_modules/uts-tencentgeolocation";
+	 
+	 import {
+	 	checkHasLottieIntegration
+	 } from "@/uni_modules/uts-animation-view";
 	 
 	export default {
 		data() {
@@ -25,6 +30,11 @@
 			gotoToast:function(e){
 				uni.navigateTo({
 					url:'/pages/SDKIntegration/Toast/Toast'
+				})
+			},
+			gotoLottie:function(e){
+				uni.navigateTo({
+					url:'/pages/SDKIntegration/Lottie/index'
 				})
 			},
 			gotoTencentLocation:function(e){
