bca10f4126526c22a7d1622a86aa142cf4973f7e
yurj26 <1816387074@qq.com>
2023-06-26 19:24:31 +0800

    feat: add uts-tests

---
+++ jest.config.js
@@ -0,0 +1,54 @@
+const {
+    readFileSync,
+    readdirSync
+} = require('fs')
+const {
+    extname,
+    resolve
+} = require('path')
+
+const describeRE = /describe\(["|'](.*)["|']/
+const testsRE = /test\(["|'](.*)["|']/g
+
+function parse(content) {
+    const describes = content.match(describeRE)
+    if (!describes) {
+        return
+    }
+    const describe = describes[1]
+    const tests = []
+    let test
+    while (test = testsRE.exec(content)) {
+        tests.push(test[1])
+    }
+    return {
+        describe,
+        tests
+    }
+}
+
+function parseDescribes() {
+    const dir = resolve(__dirname, 'uni_modules/uts-tests/utssdk')
+    const describes = []
+    readdirSync(dir).forEach(file => {
+        if (extname(file) === '.uts') {
+            describes.push(parse(readFileSync(resolve(dir, file), 'utf8')))
+        }
+    })
+    return describes
+};
+
+module.exports = {
+    testTimeout: 10000,
+    reporters: [
+        'default'
+    ],
+    globals: {
+        describes: parseDescribes()
+    },
+    watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
+    moduleFileExtensions: ['js', 'json'],
+    rootDir: __dirname,
+    testMatch: ["<rootDir>/pages/**/*test.[jt]s?(x)"],
+    testPathIgnorePatterns: ['/node_modules/']
+}
---
+++ pages/test/index.test.js
@@ -0,0 +1,37 @@
+const ERR_RE = /expected:<(.*)> but was:<(.*)>/
+
+let result;
+
+beforeAll(async () => {
+    await program.reLaunch('/pages/test/index')
+    page = await program.currentPage()
+    await page.waitFor(3000);
+    const data = await page.data('result');
+    result = data
+})
+
+function getApiFailed(describe, api) {
+    const failed = result[describe]?.failed?.find(item => {
+        return item.split(':')[0] === api
+    })
+    return failed
+}
+
+describes.forEach(d => {
+    d?.describe && describe(d.describe, () => {
+        d?.tests && d.tests.forEach(api => {
+            it(api, ()=>{
+                const failed = getApiFailed(d.describe, api)
+                if (failed) {
+                    const parts = failed.split('\n')
+                    const matches = parts[1].match(ERR_RE)
+                    if (matches?.length) {
+                        expect(matches[2]).toEqual(matches[1])
+                    } else {
+                        expect(parts[1]).toEqual('')
+                    }
+                }
+            })
+        })
+   })
+})
