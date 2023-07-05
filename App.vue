0b4903c00df29f09e7fcd6faf79abe5aeeaafa99
zhenyuWang <13641039885@163.com>
2023-07-05 16:27:49 +0800

    feat(lifecycle): onLastPageBackPress

--- App.vue
+++ App.vue
@@ -1,22 +1,29 @@
 <!-- HBuilder X 版本要求: 3.6.11+ -->
 <script>
-	export default {
-		onLaunch: function() {		
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+export default {
+  onLaunch: function () {
+    console.log('App Launch');
+  },
+  onShow: function () {
+    console.log('App Show');
+  },
+  onHide: function () {
+    console.log('App Hide');
+  },
+  onLastPageBackPress: function (): boolean | null {
+    uni.showToast({
+      title: '再按一次退出应用',
+      position: 'bottom',
+    });
+    return null;
+  },
+};
 </script>
 
 <style>
-	/*每个页面公共css */
-	/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
-	/* #ifdef APP-VUE */
-    @import './common/uni.css';
-    /* #endif */
+/*每个页面公共css */
+/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
+/* #ifdef APP-VUE */
+@import './common/uni.css';
+/* #endif */
 </style>
